2c4713434582ae1e1c83995fc731a6d5295c0127
Edward van den Brink <e.t.vandenbrink@students.uu.nl>
2021-12-15 12:42:22 +0100

    Data import through api

--- Pages/SettingsPage.axaml.cs
+++ Pages/SettingsPage.axaml.cs
@@ -19,5 +19,7 @@ namespace SoftwareProject.Pages
         {
             AvaloniaXamlLoader.Load(this);
         }
+        
+        
     }
 }
--- ViewModels/SettingsPageViewModel.cs
+++ ViewModels/SettingsPageViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using Avalonia.Controls;
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.SkiaSharpView;
@@ -9,12 +10,15 @@ using LiveChartsCore.SkiaSharpView.Painting;
 using ReactiveUI;
 using SkiaSharp;
 using SoftwareProject.Models;
+using SoftwareProject.Pages;
 
 namespace SoftwareProject.ViewModels
 {
     public class SettingsPageViewModel : ViewModelBase
     {
         private UserModel _userModel => MainWindowViewModel.User;
+        private string ticker;
+        private DateTime date;
 
         public string Username
         {
@@ -23,5 +27,30 @@ namespace SoftwareProject.ViewModels
         }
 
         public IObservable<string> Greeting => _userModel.WhenAny(x => x.Username, s => "Uw naam is: " + s.Value);
+
+        public string Ticker
+        {
+            set { ticker = value; }
+            get { return ticker; }
+        }
+        
+        public DateTime Date
+        {
+            set { date = value; }
+            get { return date; }
+        }
+        
+        public ComboBoxItem CbbItem
+        {
+            set; get;
+        }
+
+        public string? Interval => CbbItem.Content.ToString();
+
+        public void ApiImportButton()
+        {
+            ApiModel.DataImport(ticker, date, Interval);
+        }
+        
     }
 }
