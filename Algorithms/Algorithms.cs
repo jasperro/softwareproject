bfa294f81d7914d63f5aea5a39dc54f0e0c4d00c
LCBoucher <lucasboucher@gmail.com>
2021-12-23 13:09:33 +0100

    Added algorithms class

---
+++ Algorithms/Algorithms.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Avalonia.Styling;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using SoftwareProject.Types;
+
+
+namespace SoftwareProject.Algorithms
+{
+    public class Algorithms
+    {
+        /*
+        public double AverageClosingPrice(ObservableCollection<FinancialPoint> stockPoints)
+        // returns average closing price of StockPoints collection
+        /*
+        {
+            double sum = 0;
+            double average;
+            foreach (StockPoint stockPoint in stockPoints)
+            {
+                sum += stockPoint.Close;
+            }
+            average = sum / stockPoints.Count;
+            return average;
+
+        }
+        */
+        
+    }
+}
