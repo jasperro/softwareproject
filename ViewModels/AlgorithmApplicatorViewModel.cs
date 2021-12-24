e1297c751a607fd140039896d636dceaa7ef73ff
Jasper Albering <jasper@albering.nl>
2021-12-24 11:25:59 +0100

    Added algorithm selector to algorithm applicator

--- ViewModels/AlgorithmApplicatorViewModel.cs
+++ ViewModels/AlgorithmApplicatorViewModel.cs
@@ -1,19 +1,24 @@
+using System;
 using Avalonia.Controls;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace SoftwareProject.ViewModels
 {
     public class AlgorithmApplicatorViewModel : ViewModelBase
     {
-        public string ShortName
-        {
-            get;
-        } = "";
+        public string ShortName { get; } = "";
 
-        public CalendarDateRange? DateRange
-        {
-            get;
-        }
-        
+        public CalendarDateRange? DateRange { get; }
+
+        [Reactive] public ComboBoxItem? SelectedAlgorithmComboBoxItem { get; set; }
+
+        public ComboBoxItem[] AlgorithmComboBoxItems { get; } =
+            { new() { Content = "item 1" }, new() { Content = "item 2" } };
+
+        public IObservable<string> SelectedAlgorithmName =>
+            this.WhenAny(x => x.SelectedAlgorithmComboBoxItem, s => s.Value?.Content.ToString() ?? "No selection!");
+
         public AlgorithmApplicatorViewModel()
         {
         }
