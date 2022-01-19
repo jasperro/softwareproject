4d93e15ed7f9fe5410153ab34580ec92c5a64179
Jasper Albering <jasper@albering.nl>
2022-01-19 09:54:16 +0100

    Added easier selector for API import intraday

--- ViewModels/SettingsPageViewModel.cs
+++ ViewModels/SettingsPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Avalonia.Data.Converters;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using SoftwareProject.Models;
@@ -10,6 +11,12 @@ namespace SoftwareProject.ViewModels
 {
     public class SettingsPageViewModel : ViewModelBase
     {
+        public SettingsPageViewModel()
+        {
+            this.ObservableForProperty(x => x.IntradayMonthAmount).Subscribe(_ =>
+                ImportDatum = DateTimeOffset.Now.AddMonths(-IntradayMonthAmount));
+        }
+
         private UserModel _userModel => User;
 
         public string Username
@@ -43,10 +50,14 @@ namespace SoftwareProject.ViewModels
         // Time settings
         public IObservable<string> CurrentDateString =>
             Timekeeping.WhenAny(x => x.CurrentTime, _ => Timekeeping.CurrentTime.ToString());
+
         [Reactive] public DateTimeOffset? SelectedDate { get; set; }
         [Reactive] public string NewTickInterval { get; set; } = Timekeeping.TickInterval.ToString();
         [Reactive] public bool TimerRunning { get; set; } = Timekeeping.Timer.Enabled;
         [Reactive] public string NewTimeStep1Second { get; set; } = Timekeeping.TimeStep1Second.ToString();
+        [Reactive] public int IntradayMonthAmount { get; set; }
+
+        public IObservable<bool> DailySelected => this.WhenAny(x => x.Interval, s => s.Value == "daily");
 
         public void ChangeDateToSelected()
         {
