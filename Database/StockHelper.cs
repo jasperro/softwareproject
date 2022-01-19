117cb6114be933b504c91bee5373fd3a2e6fbaa2
Jasper Albering <jasper@albering.nl>
2022-01-19 14:29:03 +0100

    Implemented intraday to daily converter/query with SQLite

--- Database/StockHelper.cs
+++ Database/StockHelper.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using LiveChartsCore.Defaults;
 using Microsoft.Data.Sqlite;
 using SoftwareProject.Types;
@@ -28,14 +29,42 @@ namespace SoftwareProject
             command.ExecuteNonQuery();
         }
 
-        public Stock GetStockFromDb(string shortname)
+        public Stock GetStockFromDb(string shortname, DateTimeOffset? day = null)
         {
             var command = DatabaseConnection.CreateCommand();
             ObservableCollection<FinancialPoint> stockPoints = new();
-            command.CommandText = @"
-			    SELECT * FROM StockData WHERE StockShortName = $shortname;	
-				";
             command.Parameters.AddWithValue("$shortname", shortname);
+            // Get daily data from intraday
+            if (day == null)
+                command.CommandText = @"
+                SELECT max(High) as High,
+                       min(Low) as Low,
+                       (SELECT Open
+                        FROM StockData AS T
+                        WHERE T.DateTime = min(StockData.DateTime) AND T.StockShortName = $shortname
+                        GROUP BY T.DateTime
+                       ) AS Open,
+                       (SELECT Close
+                        FROM StockData AS T
+                        WHERE T.DateTime = max(StockData.DateTime) AND T.StockShortName = $shortname
+                        GROUP BY T.DateTime
+                       ) AS Close,
+                       max(DateTime) as DateTime
+                FROM StockData
+                WHERE StockShortName = $shortname
+                GROUP BY strftime('%Y-%m-%d', DateTime);
+                ";
+            else
+            {
+                // Get intraday data
+                command.CommandText = @"
+			    SELECT * FROM StockData
+			    WHERE StockShortName = $shortname
+                AND strftime('%Y-%m-%d', DateTime) = $day;
+			    ";
+                command.Parameters.AddWithValue("$day", day.Value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture));
+            }
+
             SqliteDataReader reader = command.ExecuteReader();
             if (reader.HasRows)
             {
@@ -59,8 +88,8 @@ namespace SoftwareProject
             reader.Close();
             return new Stock(shortname, stockPoints);
         }
-        
-        
+
+
         public void FollowStock(int id, string stock)
         {
             var command = DatabaseConnection.CreateCommand();
